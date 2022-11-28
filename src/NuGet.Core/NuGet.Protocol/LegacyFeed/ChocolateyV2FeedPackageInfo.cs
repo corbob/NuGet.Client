3f4ec13763d873b7e9febe587b179bbd3d8b01ee
Gary Ewan Park <gep13@gep13.co.uk>
2022-11-28 15:33:29 +0000

    (#9) Fix copyright headers
    To ensure that any new files are stamped with Chocolatey Software, Inc,
    and any files that we have edited have both .Net Foundation and
    Chocolatey Software, Inc.

--- src/NuGet.Core/NuGet.Protocol/LegacyFeed/ChocolateyV2FeedPackageInfo.cs
+++ src/NuGet.Core/NuGet.Protocol/LegacyFeed/ChocolateyV2FeedPackageInfo.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) 2022-Present Chocolatey Software, Inc.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 //////////////////////////////////////////////////////////
